b58742ad7a44f849611d76aa4c7b57e4cde3ffd0
mdendi6@gmail.com <mdendi6@gmail.com>
2020-06-11 20:29:23 +0700

    my portofolio

---
+++ index.js
@@ -0,0 +1,4 @@
+import './styles/js/slideshow.js';
+import './styles/js/navbar.js';
+import './styles/js/textanimasi.js';
+import "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js";
