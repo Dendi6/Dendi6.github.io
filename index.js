d64625a49f8cf45c5c041119cb2fad8052e28b91
mdendi6@gmail.com <mdendi6@gmail.com>
2020-08-27 10:19:07 +0700

    v3

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
-import './styles/js/textanimasi.js';
-import "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js";
-document.getElementById("year_now").innerHTML = new Date().getFullYear();
+import './styles/view/nav.js';
+import './styles/js/time.js';
